54415d2b41d7786b8d1f3c71c4d29e7475596f30
Toby Flemming <tobyf93@hotmail.com>
2015-05-30 15:39:15 +1000

    Create session on user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,21 @@
 # == Route Map
 #
-#     Prefix Verb   URI Pattern               Controller#Action
-#      users GET    /users(.:format)          users#index
-#            POST   /users(.:format)          users#create
-#   new_user GET    /users/new(.:format)      users#new
-#  edit_user GET    /users/:id/edit(.:format) users#edit
-#       user GET    /users/:id(.:format)      users#show
-#            PATCH  /users/:id(.:format)      users#update
-#            PUT    /users/:id(.:format)      users#update
-#            DELETE /users/:id(.:format)      users#destroy
-# demos_maps GET    /demos/maps(.:format)     demos#maps
+#           Prefix Verb   URI Pattern                           Controller#Action
+#             root GET    /                                     pages#home
+#             home GET    /home(.:format)                       pages#home
+# user_newlocation GET    /users/:user_id/newlocation(.:format) users#new_location
+#            users GET    /users(.:format)                      users#index
+#                  POST   /users(.:format)                      users#create
+#         new_user GET    /users/new(.:format)                  users#new
+#        edit_user GET    /users/:id/edit(.:format)             users#edit
+#             user GET    /users/:id(.:format)                  users#show
+#                  PATCH  /users/:id(.:format)                  users#update
+#                  PUT    /users/:id(.:format)                  users#update
+#                  DELETE /users/:id(.:format)                  users#destroy
+#            login GET    /login(.:format)                      session#new
+#                  POST   /login(.:format)                      session#create
+#                  DELETE /login(.:format)                      session#destroy
+#       demos_maps GET    /demos/maps(.:format)                 demos#maps
 #
 
 Rails.application.routes.draw do
@@ -20,6 +26,10 @@ Rails.application.routes.draw do
   resources :users do 
     get 'newlocation' => 'users#new_location'
   end
+
+  get 'login' => 'session#new'
+  post 'login' => 'session#create'
+  delete 'login' => 'session#destroy'
 
   # Demos
   get 'demos/maps' => 'demos#maps'
