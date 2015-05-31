db041bcbc0dbb4c9a04426ff0532c6283108af3a
Toby Flemming <tobyf93@hotmail.com>
2015-05-31 11:18:40 +1000

    Upload creates an item record pointing to Cloudinary file

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,22 @@
 class ItemsController < ApplicationController
   def index
+    @user = @current_user
   end
 
   def create
-    file_path = params[:file].file_path
-    Cloudinary::Uploader.upload(file_path)
+    # Get uploaded file and push it to Cloudinary
+    file = params[:file]
+    cl_info = Cloudinary::Uploader.upload(file.path)
+
+    if cl_info
+      item = Item.new
+      item.name = file.original_filename
+      item.media_type = file.content_type
+      item.latitude = @current_user.latitude
+      item.longitude = @current_user.longitude
+      item.user_id = @current_user.id
+      item.save
+    end
   end
 
   def new
--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,30 @@
 # == Route Map
 #
-#        Prefix Verb   URI Pattern                           Controller#Action
-#          root GET    /                                     pages#home
-#          home GET    /home(.:format)                       pages#home
-# user_location GET    /users/:user_id/newlocation(.:format) users#location
-#               POST   /users/:user_id/setlocation(.:format) users#location
-#         users GET    /users(.:format)                      users#index
-#               POST   /users(.:format)                      users#create
-#     edit_user GET    /users/:id/edit(.:format)             users#edit
-#          user GET    /users/:id(.:format)                  users#show
-#               PATCH  /users/:id(.:format)                  users#update
-#               PUT    /users/:id(.:format)                  users#update
-#               DELETE /users/:id(.:format)                  users#destroy
-#        signup GET    /signup(.:format)                     users#new
-#         login GET    /login(.:format)                      session#new
-#               POST   /login(.:format)                      session#create
-#        logout DELETE /logout(.:format)                     session#destroy
-#    demos_maps GET    /demos/maps(.:format)                 demos#maps
+#         Prefix Verb   URI Pattern                              Controller#Action
+#           root GET    /                                        pages#home
+#           home GET    /home(.:format)                          pages#home
+#  user_location GET    /users/:user_id/location(.:format)       users#new_location
+#                POST   /users/:user_id/location(.:format)       users#set_location
+#     user_items GET    /users/:user_id/items(.:format)          items#index
+#                POST   /users/:user_id/items(.:format)          items#create
+#  new_user_item GET    /users/:user_id/items/new(.:format)      items#new
+# edit_user_item GET    /users/:user_id/items/:id/edit(.:format) items#edit
+#      user_item GET    /users/:user_id/items/:id(.:format)      items#show
+#                PATCH  /users/:user_id/items/:id(.:format)      items#update
+#                PUT    /users/:user_id/items/:id(.:format)      items#update
+#                DELETE /users/:user_id/items/:id(.:format)      items#destroy
+#          users GET    /users(.:format)                         users#index
+#                POST   /users(.:format)                         users#create
+#      edit_user GET    /users/:id/edit(.:format)                users#edit
+#           user GET    /users/:id(.:format)                     users#show
+#                PATCH  /users/:id(.:format)                     users#update
+#                PUT    /users/:id(.:format)                     users#update
+#                DELETE /users/:id(.:format)                     users#destroy
+#         signup GET    /signup(.:format)                        users#new
+#          login GET    /login(.:format)                         session#new
+#                POST   /login(.:format)                         session#create
+#         logout DELETE /logout(.:format)                        session#destroy
+#     demos_maps GET    /demos/maps(.:format)                    demos#maps
 #
 
 Rails.application.routes.draw do
