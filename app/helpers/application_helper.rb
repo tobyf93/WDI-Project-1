cb4f9697ef783d842c2320b98faec0c7cc894db9
Toby Flemming <tobyf93@hotmail.com>
2015-05-31 13:14:00 +1000

    Logo to navbar.  Navbar options created by application_helper.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,40 @@
 module ApplicationHelper
+  def navigation
+    html = ''
+
+    html << user_nav
+    html << logged_out_nav
+
+    "<ul class='nav navbar-nav pull-right'>#{html}</ul>"
+  end
+
+  private
+  def user_nav
+    html = ''
+
+    if @current_user.present?
+      html << "<p class='navbar-text'>Welcome #{@current_user.username}!</p>"
+      html << "<li><a href='#{users_path}'>View Users</a></li>"
+      html << "<li><a href='#{new_user_item_path @current_user}'>Create Item</a></li>"
+      html << "<li><a href='#{user_items_path @current_user}'>All Items</a></li>"
+      html << "<li><a href='#{user_location_path @current_user}'>Edit Location</a></li>"
+      html << "<li>#{button_to 'Logout', logout_path, :form_class => 'navbar-form', :class => 'btn btn-default', :method => :delete}</li>"
+    end
+
+    html
+  end
+
+  def admin_nav
+  end
+
+  def logged_out_nav
+    html = ''
+
+    if !@current_user
+      html << "<li><a href='#{signup_path}'>Sign Up</a></li>"
+      html << "<li><a href='#{login_path}'>Login</a></li>"
+    end
+
+    html
+  end
 end
