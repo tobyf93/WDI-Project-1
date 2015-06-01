26adea9a684ca7833c38c6717a02f88a9f6d9db8
Toby Flemming <tobyf93@hotmail.com>
2015-06-01 20:42:58 +1000

    File selection working

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,14 +1,19 @@
 $(document).ready(function() {
-  $('html').on('click', 'a', 0);
+  $('html').on('click', '#fileList a', previewFile);
 });
 
 var previewFile = function() {
-  var cl_id = $(this).attr('data-cl');
-  console.log(cl_id);
+  var $this = $(this);
+  var cl_id = $this.attr('data-cl');
 
   if (cl_id) {
     $img = $('#filePreview img');
     var filePath = 'http://res.cloudinary.com/ddfua301o/image/upload/' + cl_id;
     $img.attr('src', filePath);
+
+    $('#fileList a').removeClass('active');
+    $this.addClass('active');
+  } else {
+    $this.remove();
   }
 };
